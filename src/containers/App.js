34f33c16c4cd42ae34c16a06fca2dc3b3faf6fbf
Hoang Van Duc <duch52362@gmail.com>
2024-03-23 21:04:40 +0700

    create HomeHeader #44

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,8 +16,7 @@ import Header from './Header/Header';
 import System from '../routes/System';
 
 import { CustomToastCloseButton } from '../components/CustomToast';
-import ConfirmModal from '../components/ConfirmModal';
-
+import Homepage from './HomePageFD/HomePage.js';
 class App extends Component {
     handlePersistorState = () => {
         const { persistor } = this.props;
@@ -42,7 +41,6 @@ class App extends Component {
             <Fragment>
                 <Router history={history}>
                     <div className="main-container">
-                        <ConfirmModal />
                         {this.props.isLoggedIn && <Header />}
 
                         <span className="content-container">
@@ -50,6 +48,7 @@ class App extends Component {
                                 <Route path={path.HOME} exact component={Home} />
                                 <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
                                 <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
+                                <Route path={path.HOMEPAGE} component={Homepage} />
                             </Switch>
                         </span>
 
